1cadf33a9f3f3168cede8d8f583825b45ed72c69
owner.laptop <wang.benny0102@gmail.com>
2022-05-09 15:44:43 +0800

    220509 fixing hner get_ne

--- ner/ner.py
+++ ner/ner.py
@@ -61,33 +61,51 @@ class HealthNER:
 
         params
             setence: str, sentence 
-
+        return
+            list<str>
         '''
-        decoding = []
-        isNumber = False
-        chunk = ''
-        for i in range(len(sentence)):
-            if sentence[i].isnumeric():
-                isNumber = True
-                chunk += sentence[i]
-            elif sentence[i] in ['.', ',']:
-                isNumber = False
-                chunk += sentence[i]
-                decoding.append(chunk)
-                chunk = ''
-            else:
-                if isNumber:
-                    isNumber = False
-                    decoding.append(chunk)
-                    chunk = ''
-                decoding.append(sentence[i])
+        # decoding = []
+        # isNumber = False
+        # chunk = ''
+        # for i in range(len(sentence)):
+        #     if sentence[i].isnumeric():
+        #         isNumber = True
+        #         chunk += sentence[i]
+        #     elif sentence[i] in ['.', ',']:
+        #         isNumber = False
+        #         chunk += sentence[i]
+        #         decoding.append(chunk)
+        #         chunk = ''
+        #     else:
+        #         if isNumber:
+        #             isNumber = False
+        #             decoding.append(chunk)
+        #             chunk = ''
+        #         decoding.append(sentence[i])
+
+        # return decoding
+        encoding = self.tokenizer.encode(sentence, return_offsets_mapping=True,
+                                         padding='max_length',
+                                         truncation=True,
+                                         max_length=128,)
+
+        unk_lst = [i for i, x in enumerate(encoding) if x == 100]
+
+        decoding = self.tokenizer.decode(
+            encoding[1:encoding.index(102)]).split(' ')
+
+        for unk_idx in unk_lst:
+            decoding[unk_idx] = encoding[unk_idx]
 
         return decoding
-        # encoding = self.tokenizer.encode(sentence, return_offsets_mapping=True,
-        #                                  padding='max_length',
-        #                                  truncation=True,
-        #                                  max_length=128,)
-        # decoding= self.tokenizer.decode(encoding[1:encoding.index(102)]).split(' ')
+
+        # count = 0
+        # for idx_words, words in enumerate(decoding):
+        #     for idx_word, word in enumerate(words):
+        #         c += 1
+        #         if word== '[':
+        #             if len(words)>= idx_word+ 3:
+        #                 if '[UNK]' in words[idx_words][idx_word:idx_word+3]:
 
     def get_ne(self, sentence):
         '''get named entity
@@ -133,13 +151,15 @@ class HealthNER:
 # sentence = '我媽媽查出有心臟病，還有早搏，醫生給他開了穩心顆粒和鹽酸美西律片，吃了以後就噁心，嘔吐，頭暈，手腳無力，還顫動，是怎麼回事，已經兩個多小時了，有危險嗎？，'
 # sentence = '眼底病變：當微細動脈硬化會導致動脈內腔變細，動脈內壁變厚，使微細動脈出血，視神經乳頭浮腫，造成患者視力逐漸減低。但患者大多是再出現視力模糊後，接受眼科醫師檢查時，才發現罹患高血壓疾病。'
 # # sentence = '懷孕53.天，有2.5次自然流產是不是正常'
-# print(len(sentence))
-# encoding = hner.tokenizer.encode(sentence, return_offsets_mapping=True,
-#                                  padding='max_length',
-#                                  truncation=True,
-#                                  max_length=128,)
-# print(encoding)
-# print(hner.get_decoding(sentence))
-# print(hner._get_model_output(sentence))
-# print(hner.get_ne(sentence))
-# # %%
+# sentence= '（五） 返家後若有發燒、腹痛厲害或嚴重嘔吐、腹瀉應立即返診。'
+# # print(len(sentence))
+# # encoding = hner.tokenizer.encode(sentence, return_offsets_mapping=True,
+# #                                  padding='max_length',
+# #                                  truncation=True,
+# #                                  max_length=128,)
+# # print(encoding)
+# # print(hner.get_decoding(sentence))
+# # print(hner._get_model_output(sentence))
+# for symp in hner.get_ne(sentence):
+#     print(symp)
+# %%
